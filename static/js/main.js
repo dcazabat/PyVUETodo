575c5f0bfd99f0434e6ea108358405293a507e84
dcazabat <dacazabat@gmail.com>
2022-06-20 21:11:19 -0300

    Change Flask-SQLAlch with SQLlch ORM / Vue Comp's

--- static/js/main.js
+++ static/js/main.js
@@ -1,54 +1,60 @@
-const { createApp } = Vue
+import { createApp } from 'vue'
+import taskComponent from './taskComponent.js'
 
-createApp({
-  data() {
-    return {
-      task: {
-        title: '',
-      },
-      tasks: [],
-    }
-  },
-  async created(){
-    await this.getTasks()
-  },
-  methods: {
-    async sendRequest(url, method, data){
-      const my_headers = new Headers({
-        'Content-type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-      })
-      // console.log(url, my_headers, data)
-      const reponse = await fetch(url, {
-        method: method,
-        headers: my_headers,
-        body: data,
-      })
-      return reponse
-    },
-    async getTasks() {
-        const reponse = await this.sendRequest(window.location, 'get')
-        this.tasks = await reponse.json()
-    },
-    async addTask() {
-        await this.getTasks()
 
-        await this.sendRequest(window.location + 'add', 'post', JSON.stringify(this.task))
+createApp(taskComponent).mount('#app')
 
-        await this.getTasks()
+// const { createApp } = Vue
 
-        this.task.title = ''
-    },
-    async deleteTask(task) {
-      await this.sendRequest(window.location + 'delete','post', JSON.stringify(task))
+// createApp({
+//   data() {
+//     return {
+//       task: {
+//         title: '',
+//       },
+//       tasks: [],
+//     }
+//   },
+//   async created(){
+//     await this.getTasks()
+//   },
+//   methods: {
+//     async sendRequest(url, method, data){
+//       const my_headers = new Headers({
+//         'Content-type': 'application/json',
+//         'X-Requested-With': 'XMLHttpRequest'
+//       })
+//       // console.log(url, my_headers, data)
+//       const reponse = await fetch(url, {
+//         method: method,
+//         headers: my_headers,
+//         body: data,
+//       })
+//       return reponse
+//     },
+//     async getTasks() {
+//         const reponse = await this.sendRequest(window.location, 'get')
+//         this.tasks = await reponse.json()
+//     },
+//     async addTask() {
+//         await this.getTasks()
 
-      await this.getTasks()
-    },
-    async completeTask(task) {
-      await this.sendRequest(window.location + 'complete','post', JSON.stringify(task))
+//         await this.sendRequest(window.location + 'add', 'post', JSON.stringify(this.task))
 
-      await this.getTasks()
-    },
-  },
-  delimiters: ['{', '}']
-}).mount('#app')
+//         await this.getTasks()
+
+//         this.task.title = ''
+//     },
+//     async deleteTask(task) {
+//       await this.sendRequest(window.location + 'delete','post', JSON.stringify(task))
+
+//       await this.getTasks()
+//     },
+//     async completeTask(task) {
+//       await this.sendRequest(window.location + 'complete','post', JSON.stringify(task))
+
+//       await this.getTasks()
+//     },
+//   },
+//   delimiters: ['{', '}']
+// }).mount('#app')
